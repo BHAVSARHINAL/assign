2d04ae37eb086d1241b63ed9f98382441bf56801
BHAVSARHINAL <110092239+BHAVSARHINAL@users.noreply.github.com>
2022-08-20 19:48:07 +0530

    Add files via upload

---
+++ useifelse.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+printf("enter a charater");
+scanf("%c",&ch);
+
+ if((ch>='a')&&(ch<='z'))
+{
+	printf("enter a alphabet");
+	
+	}	
+else 
+{
+
+ printf("enter  a number");	
+}
+               return 0;
+}

7ab16b35b0f3e7dca6c1a1347f1d5f4c69828c18
BHAVSARHINAL <110092239+BHAVSARHINAL@users.noreply.github.com>
2022-08-23 16:59:18 +0530

    Add files via upload

---
+++ ternary_pre.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{ 
+  int x,y,z;
+  
+  int max;
+  
+  printf("enter any three num : \n");
+ // scanf("%d %d %d",&x ,&y,&z);
+  
+  int;
+ x=6,y=50,z=400;
+  max =(x>y && x>z) ? x:(y>z) ? y:z;
+  printf("max num =%d", max);
+  
+  max =(x>y) ? (x>z) ?x:z :(y>z) ? y:z;
+    
+    printf("max out of %d ,%d and %d is : %d",x,y,z,max);
+  
+  
+  return 0;
+  
+  
+  
+  
+  
+	
+}

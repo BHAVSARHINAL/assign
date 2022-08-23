f7cb836869ef91e6f5bf16e77d050009ddbf2059
BHAVSARHINAL <110092239+BHAVSARHINAL@users.noreply.github.com>
2022-08-23 20:11:55 +0530

    Add files via upload

---
+++ maxnum.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+	
+	int num1,num2;
+	
+	printf("enter two int num..");
+	scanf("%d %d",&num1,&num2);
+	
+	
+	printf("\n you have enter num 1=%d and num 2 =%d\n ",num1 ,num2);
+	
+	if(num1>num2)
+	{
+		printf("max num =%d", num1);
+		
+	}
+	 else
+	 	{
+		 
+		 printf("max num =%d", num2);
+	 }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}

1000a4efed013ea2cfd41a867f018b1c9347c9d5
BHAVSARHINAL <110092239+BHAVSARHINAL@users.noreply.github.com>
2022-08-25 19:36:18 +0530

    Add files via upload

---
+++ swapno.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int a=9,b=5,c;
+        //c=a;//9
+    	//a=b;//5
+	    //b=c;//2
+	
+	
+	c=a+b;//14
+	b=a-b;//9
+	a=a-b;//5
+	
+	printf("a=%d b=%d",a,b);
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}

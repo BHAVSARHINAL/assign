97d731e96cc88e4c2c5bd27fff4eddbe47980e4d
BHAVSARHINAL <110092239+BHAVSARHINAL@users.noreply.github.com>
2022-08-25 22:32:54 +0530

    Add files via upload

---
+++ continue.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int test()
+{
+	int i;
+	for(i=1;i<=8;i++);
+{
+	if(i==1||i==2||i==10)
+	{
+	 continue;
+   }
+	printf("%d\n",i);
+}
+}
+int main()
+{
+	    test();
+		return 0;
+}

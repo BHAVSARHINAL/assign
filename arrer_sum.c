58b9c62fd6017b69211f100468b168d95b146c9b
BHAVSARHINAL <110092239+BHAVSARHINAL@users.noreply.github.com>
2022-09-01 22:13:57 +0530

    Add files via upload

---
+++ arrer_sum.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+int main()
+{
+	int a[2],b[2],j;
+	
+	printf("enter a first arrey num: \n");
+	for(j=0;j<2;j++)
+	{
+		scanf("%d",&a[j]);
+	}
+	
+
+	printf("enter a second arrey num: \n");
+	for(j=0;j<2;j++)
+	{
+		scanf("%d",&b[j]);
+	}	
+	
+	
+	printf("\n sum of two arreys :\n");
+	for(j=0;j<2;j++)
+	{
+		printf("%d\n",a[j]+b[j]);
+		
+	}
+	
+	//printf("%d",a[j]+b[j]);
+	
+	
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
